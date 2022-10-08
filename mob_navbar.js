36a332d8f96fcc69a685e9cbc2788c99a421f51d
jramso <josue.rsou2@gmail.com>
2022-10-08 16:28:36 -0300

    update

--- mob_navbar.js
+++ mob_navbar.js
@@ -2,8 +2,8 @@ const btn = document.getElementById("btn-mobile");
 
 function toggleMenu(){
     
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("active")
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("active");
 }
 
-btn.addEventListener("click",toggleMenu());
+btn.addEventListener('click',toggleMenu);
